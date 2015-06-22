cf3b1d856ff58131e78c3bae6364947db3a15947
Zuhayr <AGMogett@gmail.com>
2015-06-22 16:48:47 +0930

    Updating the maptools.

--- tools/mapmerge/2clean_map.bat
+++ tools/mapmerge/2clean_map.bat
@@ -1,8 +1,8 @@
-SET z_levels=6
+SET z_levels=1
 cd 
 
 FOR /L %%i IN (1,1,%z_levels%) DO (
-  java -jar MapPatcher.jar -clean ../../maps/exodus-%%i.dmm.backup ../../maps/exodus-%%i.dmm ../../maps/exodus-%%i.dmm
+  java -jar MapPatcher.jar -clean ../../maps/europa-%%i.dmm.backup ../../maps/europa-%%i.dmm ../../maps/europa-%%i.dmm
 )
 
 pause
