4889f47f0dcdee7653be1a971780d35e94d2b59a
Zuhayr <AGMogett@gmail.com>
2015-12-03 21:13:55 +1030

    Removed the testing Box map. Fuck it, we'll do it live.

--- tools/mapmerge/2clean_map.bat
+++ tools/mapmerge/2clean_map.bat
@@ -3,7 +3,6 @@ cd
 
 FOR /L %%i IN (1,1,%z_levels%) DO (
   java -jar MapPatcher.jar -clean ../../maps/europa-%%i.dmm.backup ../../maps/europa-%%i.dmm ../../maps/europa-%%i.dmm
-  java -jar MapPatcher.jar -clean ../../maps/box-%%i.dmm.backup ../../maps/box-%%i.dmm ../../maps/box-%%i.dmm
 )
 
 pause
